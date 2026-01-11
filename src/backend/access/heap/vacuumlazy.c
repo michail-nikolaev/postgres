b950771049962fe0aa0471ceb5c6b5c46d75fdd5
Mikhail Nikalayeu <mihailnikalayeu@gmail.com>
2026-01-11 19:27:52 +0300

    Add STIR access method and flags related to auxiliary indexes
    This patch provides infrastructure for following enhancements to concurrent index builds by:
    - ii_Auxiliary in IndexInfo: indicates that an index is an auxiliary index used during concurrent index build
    - validate_index in IndexVacuumInfo: set if index_bulk_delete called during the validation phase of concurrent index build
    - STIR (Short-Term Index Replacement) access method is introduced, intended solely for short-lived, auxiliary usage
    
    STIR functions are designed as an ephemeral helper during concurrent index builds, temporarily storing TIDs without providing the full features of a typical access method. As such, it raises warnings or errors when accessed outside its specialized usage path.
    
    Planned to be used in following commits.

--- src/backend/access/heap/vacuumlazy.c
+++ src/backend/access/heap/vacuumlazy.c
@@ -3168,6 +3168,7 @@ lazy_vacuum_one_index(Relation indrel, IndexBulkDeleteResult *istat,
 	ivinfo.message_level = DEBUG2;
 	ivinfo.num_heap_tuples = reltuples;
 	ivinfo.strategy = vacrel->bstrategy;
+	ivinfo.validate_index = false;
 
 	/*
 	 * Update error traceback information.
@@ -3219,6 +3220,7 @@ lazy_cleanup_one_index(Relation indrel, IndexBulkDeleteResult *istat,
 
 	ivinfo.num_heap_tuples = reltuples;
 	ivinfo.strategy = vacrel->bstrategy;
+	ivinfo.validate_index = false;
 
 	/*
 	 * Update error traceback information.
