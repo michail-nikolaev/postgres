86ac50b2d2d99dfe23a14051d7217d4d2f531ea4
Antonin Houska <ah@cybertec.at>
2026-01-22 09:23:11 +0100

    Add REPACK command
    REPACK absorbs the functionality of VACUUM FULL and CLUSTER in a single
    command.  Because this functionality is completely different from
    regular VACUUM, having it separate from VACUUM makes it easier for users
    to understand; as for CLUSTER, the term is heavily overloaded in the
    IT world and even in Postgres itself, so it's good that we can avoid it.
    
    This also adds pg_repackdb, a new utility that can invoke the new
    commands.  This is heavily based on vacuumdb.
    
    Author: Antonin Houska <ah@cybertec.at>
    Co-authored-by: Álvaro Herrera <alvherre@kurilemu.de>
    Reviewed-by: Mihail Nikalayeu <mihailnikalayeu@gmail.com>
    Reviewed-by: Robert Treat <rob@xzilla.net>
    Reviewed-by: Euler Taveira <euler@eulerto.com>
    Reviewed-by: Matheus Alcantara <matheusssilv97@gmail.com>
    Reviewed-by: Junwang Zhao <zhjwpku@gmail.com>
    Reviewed-by: jian he <jian.universality@gmail.com>
    Discussion: https://postgr.es/m/82651.1720540558@antos
    Discussion: https://postgr.es/m/202507262156.sb455angijk6@alvherre.pgsql

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -351,7 +351,6 @@ ExecVacuum(ParseState *pstate, VacuumStmt *vacstmt, bool isTopLevel)
 		}
 	}
 
-
 	/*
 	 * Sanity check DISABLE_PAGE_SKIPPING option.
 	 */
@@ -2289,8 +2288,9 @@ vacuum_rel(Oid relid, RangeVar *relation, VacuumParams params,
 			if ((params.options & VACOPT_VERBOSE) != 0)
 				cluster_params.options |= CLUOPT_VERBOSE;
 
-			/* VACUUM FULL is now a variant of CLUSTER; see cluster.c */
-			cluster_rel(rel, InvalidOid, &cluster_params);
+			/* VACUUM FULL is a variant of REPACK; see cluster.c */
+			cluster_rel(REPACK_COMMAND_VACUUMFULL, rel, InvalidOid,
+						&cluster_params);
 			/* cluster_rel closes the relation, but keeps lock */
 
 			rel = NULL;
